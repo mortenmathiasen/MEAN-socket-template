33411b20b7fc646a65174439367a74490b496592
Morten Mathiasen <morten@tranebaerkaeret.dk>
2017-04-03 12:10:02 +0200

    Fix path for windows

--- app.js
+++ app.js
@@ -20,11 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/blog', blog);
 app.use('/users', users);
-app.use(express.static('client/dist'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
