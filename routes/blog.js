63f4ced46d8fd60e9d113c4afaad4e75d464ff89
mortenmathiasen <morten@tranebaerkaeret.dk>
2017-10-31 22:22:01 +0100

    Leave at maximum 10 blog messages in database

--- routes/blog.js
+++ routes/blog.js
@@ -18,17 +18,33 @@ router.get('/get', function(req, res, next) {
 /* POST single blog post */
 router.post('/post', function(req, res, next) {
     var instance = new schema.Blog(req.body);
-/** Example post body:
-  {
-    "author": "Morten Mathiasen",
-    "body": "Hello everyone"
-  }
- **/
-     instance.save(function (err, Blog) {
-       result = err?err:Blog;
-       res.send(result);
-       return result;
-     });
+    /** Example post body:
+     {
+       "author": "Morten Mathiasen",
+       "body": "Hello everyone"
+     }
+     **/
+
+    console.log("Hallo 1");
+
+    instance.save(function (err, Blog) {
+        result = err?err:Blog;
+
+        schema.Blog.find({}).sort({_id:-1}).skip(10).exec(function (err, blogs) {
+            console.log("Hallo 2");
+            if (err)
+                return console.error(err);
+            console.log("Loader success: ", blogs);
+            blogs.forEach(function(blog){
+                console.log("Loader success: ", blog);
+                schema.Blog.findByIdAndRemove(blog._id).exec();
+            });
+
+            res.send(result);
+        });
+
+        return result;
+    });
 });
 
 //export the router
