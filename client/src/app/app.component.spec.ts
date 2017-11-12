1a4736d66db37affdcfe5d10e92e96e15242843f
mortenmathiasen <morten@tranebaerkaeret.dk>
2017-11-12 10:09:26 +0100

    Adjust test to correct title

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -24,16 +24,16 @@ describe('App: MEAN2', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'MEAN app with Angular'`, async(() => {
+  it(`should have as title 'MEAN app with Socket IO'`, async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('MEAN app with Angular');
+    expect(app.title).toEqual('MEAN app with Socket IO');
   }));
 
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('MEAN app with Angular');
+    expect(compiled.querySelector('h1').textContent).toContain('MEAN app with Socket IO');
   }));
 });
